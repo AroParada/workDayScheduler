f6ed3a90ac86e790b636136d952a2cf1f2fd61f9
harold parada <haroldparada20@gmail.com>
2020-10-05 00:42:38 -0400

    added buttons and text input

--- script.js
+++ script.js
@@ -5,5 +5,14 @@ $("#currentDay").text(moment().format("dddd, MMMM Do YYYY"))
 // var currentHour= moment().hours();
 // console.log("currentHour", currentHour) 
 
-$("#time").addClass("hour");
+
+$(".col").addClass("hour");
+
+$(".col3").addClass("saveBtn sBtni:hover ")
+
+$(".col-10 ").addClass("description")
 });
+
+ 
+// {/* <input type="text" class="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default">
+// </div>  */}
