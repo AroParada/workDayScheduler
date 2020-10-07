3d49bcc55499a7eac02592ecf9b47704f79d7aee
harold parada <haroldparada20@gmail.com>
2020-10-06 23:43:27 -0400

    adding save function

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
+
+
 $(document).ready(function() {
 
 $("#currentDay").text(moment().format("dddd, MMMM Do YYYY"))
 
-// var currentHour= moment().hours();
-// console.log("currentHour", currentHour) 
+var currentHour= moment().hours();
+console.log("currentHour", currentHour) 
 
 
 $(".col").addClass("hour");
 
 $(".col3").addClass("saveBtn sBtni:hover ")
 
-$(".col-10 ").addClass("description")
+$(".col-10 ").addClass("past")
+
+$('.save').on('click', function (){
+    console.log( $( this ).siblings(".task") )
 });
 
- 
-// {/* <input type="text" class="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default">
-// </div>  */}
+});
+
